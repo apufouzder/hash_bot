a1e38c7792f18dd1fb595d42e6d021cbfe7621a3
apufouzder <apufouzdar@gmail.com>
2024-09-26 22:23:07 +0600

    Get User Data

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,24 @@ function App() {
 
   return (
     <main>
-    
+      {userData ? (
+         <>
+         <h1 className="text-2xl font-bold mb-4">User Data</h1>
+         <ul>
+           <li>ID: {userData.id}</li>
+           <li>First Name: {userData.first_name}</li>
+           <li>Last Name: {userData.last_name || 'N/A'}</li>
+           <li>Username: {userData.username || 'N/A'}</li>
+           <li>Language Code: {userData.language_code}</li>
+           <li>Is Premium: {userData.is_premium ? 'Yes' : 'No'}</li>
+          </ul>
+          <div>
+            {userData}
+          </div>
+       </>
+      ): (
+          <p>Loading...!</p>
+    )}
     </main>
   )
 }
